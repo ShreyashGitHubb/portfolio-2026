5d9e57ef236a80aed82463116ae717cfb93a5bd8
ShreyashGitHubb <shreyasvishwakarma00@gmail.com>
2026-05-08 14:52:27 +0530

    add react vite plugin back to fix ReferenceError

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [
     tanstackStart(),
+    react(),
     tsconfigPaths(),
   ],
 });
