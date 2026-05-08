0b1582dac7d9647b931050e9e90f40f129979039
ShreyashGitHubb <shreyasvishwakarma00@gmail.com>
2026-05-08 14:23:30 +0530

    fix vercel deployment using api route

---
+++ api/index.js
@@ -0,0 +1,5 @@
+import server from '../dist/server/server.js';
+
+export default function(req) {
+  return server.fetch(req);
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,10 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
     tanstackStart(),
-    TanStackRouterVite(),
-    react(),
     tsconfigPaths(),
   ],
 });
