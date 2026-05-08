036dccae79fa07228144968a16a84adf7885996a
ShreyashGitHubb <shreyasvishwakarma00@gmail.com>
2026-05-08 18:42:12 +0530

    add tailwindcss vite plugin to fix uncompiled CSS

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     tanstackStart(),
     react(),
     tsconfigPaths(),
